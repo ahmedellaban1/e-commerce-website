52053131b1b1274f857528401c5f9cda343a763a
ahmedellaban1 <ahmedbale112001@gmail.com>
2023-08-18 21:24:44 +0300

    -

--- product/urls.py
+++ product/urls.py
@@ -3,7 +3,7 @@ from .views import home_page, shop_page, product_details
 app_name = 'product'
 urlpatterns = [
     path('', home_page, name='home-page'),
-    path('<int:id><str:slug>', product_details, name='product_details'),
+    path('<int:id>-<str:slug>', product_details, name='product_details'),
     path('shop', shop_page, name='shop'),
 
 ]
